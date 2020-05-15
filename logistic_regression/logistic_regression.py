dd51d97e0559f63e4a633ec10c25537eb236ad94
Marek Kancir <kancir.marek@gmail.com>
2020-05-15 16:43:11 +0200

    add class LogisticRegression

---
+++ logistic_regression/logistic_regression.py
@@ -0,0 +1,11 @@
+
+import numpy as np
+
+class LogisticRegression():
+    def __init__(self, max_iter=100, learning_rate=0.1, reg_lambda=0.5, beta=0.9):
+        self.iter = max_iter
+        self.lr = learning_rate
+        self.reg_lambda = reg_lambda
+        self.b = beta
+        
+    
