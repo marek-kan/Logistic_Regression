a5f5c1de871800ceb9f1956d52755cf1c1d3ba32
Marek Kancir <kancir.marek@gmail.com>
2020-05-15 17:20:19 +0200

    add update method

--- logistic_regression/logistic_regression.py
+++ logistic_regression/logistic_regression.py
@@ -8,10 +8,23 @@ class LogisticRegression():
         self.reg_lambda = reg_lambda
         self.b = beta
         
-    def h(z):
+    def h(x, w):
+        z = np.dot(x, w)
         return 1/(1 + np.exp(-z))
     
     def cost(self, x, y, m):
-        c = 1/m * (-y * np.log(self.h(self.w * x)) - (1-y)*np.log(1-self.h(self.w * x)))
+        c = 1/m * (-y * np.log(self.h(x, self.w)) - (1-y)*np.log(1-self.h(x, self.w)))
         regC = c + self.reg_lambda/(2*m) * np.dot(self.w.T, self.w**2)
         return sum(regC)
+    
+    def update(self, x, y, m):
+        pred = self.h(x, self.w)
+        grad0 = 1/m * np.dot(self.w[:, 0].T, sum(pred-y))
+        grad1 = 1/m * np.dot(self.w[:, 1:].T, sum(pred-y))
+        
+        self.v[0] = self.b*self.v[0] + (1-self.b)*grad0
+        self.v[1:] = self.b*self.v[1:] + (1-self.b)*grad1
+        self.w[0] = self.w[0] - self.lr*self.v[0]
+        self.w[1:] = self.w[1:] * (1 - self.lr*self.reg_lambda/m) - self.lr*self.v[1:]
+        
+        
