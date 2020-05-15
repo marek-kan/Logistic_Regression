e1a7271c6f7746499d9463a39b49adc297e69c70
Marek Kancir <kancir.marek@gmail.com>
2020-05-15 18:31:41 +0200

    test binary on stratified kfold

--- test_binary.py
+++ test_binary.py
@@ -1,32 +1,54 @@
-
+import numpy as np
 from sklearn.datasets import load_breast_cancer
 from sklearn.linear_model import LogisticRegression as LR
 from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedKFold
 from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 
 from logistic_regression import logistic_regression as lr
+np.random.seed(5)
 
 x, y = load_breast_cancer(return_X_y=True)
 scaler = StandardScaler()
 
-x_tr, x_te, y_tr, y_te = train_test_split(x, y, test_size=0.1, random_state=5, shuffle=True)
-x_tr = scaler.fit_transform(x_tr)
-x_te = scaler.transform(x_te)
+kf = StratifiedKFold(10, shuffle=True, random_state=0)
+acc1 = []
+acc2 = []
+for train_index, validation_index in kf.split(x, y):
+    scaler = StandardScaler()
+    y_tr,y_te = y[train_index], y[validation_index]
+    x_tr,x_te = x[train_index], x[validation_index]
+    x_tr = scaler.fit_transform(x_tr)
+    x_te = scaler.transform(x_te)
+    
+    log = lr.LogisticRegression(max_iter=5000, reg_lambda=1e-4, learning_rate=0.15, beta=0.8)
+    log.fit(x_tr, y_tr)
+    sk = LR()
+    sk.fit(x_tr, y_tr)
+    
+    temp = accuracy_score(y_te, log.predict(x_te))
+    acc1.append(temp)
+    temp = accuracy_score(y_te, sk.predict(x_te))
+    acc2.append(temp)
+    
 
-log = lr.LogisticRegression(max_iter=5000, reg_lambda=0, learning_rate=0.01)
-log.fit(x_tr, y_tr)
-
-pred = log.predict(x_te)
-acc1 = accuracy_score(y_te, pred)
-
-sk = LR(tol=1e-12)
-sk.fit(x_tr, y_tr)
-acc2 = accuracy_score(y_te, sk.predict(x_te))
+print(f'Custom LogReg mean acc: {np.mean(acc1)}')
+print(f'Sklearn LogReg mean acc: {np.mean(acc2)}')
 
 costs = log.costs
 plt.plot(range(len(costs)), costs)
 plt.title('Training loss')
 plt.show()
 plt.close()
+
+# Test "online learning"
+new_examples = 20
+online_acc = []
+for i in range(new_examples):
+    log.fit_sample(x_te[i, :].reshape(1, x.shape[1]), y_te[i], iterations=2)
+    online_acc.append(accuracy_score(y_te[i+1:], log.predict(x_te[i+1:, :])))
+    
+plt.plot(range(len(online_acc)), online_acc)
+plt.title('MAE after online learning')
+plt.show()
